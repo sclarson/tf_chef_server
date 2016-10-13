00456e96ba84fde1a8f264984b5b37dbaab3b7d0
sclarson <Seth.larson@gmail.com>
2016-10-13 09:51:24 -0500

    Add missing dependency compat_resource
    The chef-client run was failing to converge due to the compat_resource cookbook dependency not resolving.

--- files/chef-cookbooks.sh
+++ files/chef-cookbooks.sh
@@ -6,6 +6,7 @@ sudo rm -rf /var/chef/ssl       ; sudo mkdir -p /var/chef/ssl
 
 for DEP in apt apt-chef chef-client     ; do curl -sL https://supermarket.chef.io/cookbooks/${DEP}/download | sudo tar xzC /var/chef/cookbooks; done
 for DEP in chef_handler chef-ingredient ; do curl -sL https://supermarket.chef.io/cookbooks/${DEP}/download | sudo tar xzC /var/chef/cookbooks; done
+for DEP in compat_resource              ; do curl -sL https://supermarket.chef.io/cookbooks/${DEP}/download | sudo tar xzC /var/chef/cookbooks; done
 for DEP in chef-server chef-sugar cron  ; do curl -sL https://supermarket.chef.io/cookbooks/${DEP}/download | sudo tar xzC /var/chef/cookbooks; done
 for DEP in firewall hostsfile logrotate ; do curl -sL https://supermarket.chef.io/cookbooks/${DEP}/download | sudo tar xzC /var/chef/cookbooks; done
 for DEP in packagecloud system yum      ; do curl -sL https://supermarket.chef.io/cookbooks/${DEP}/download | sudo tar xzC /var/chef/cookbooks; done
